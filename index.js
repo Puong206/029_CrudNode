e56d517a41969022c3180615cebc33cb73d0a450
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-10-19 12:16:35 +0700

    index

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Mobil from './Mobil.js';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Ang Ang Ang');
+});
+
+app.use(bodyParser.json());
+app.use('/Mobil', Mobil);
+
+app.listen(PORT, () => 
+    console.log(`Server berjalan di port: http://localhost:${PORT}`)
+);
